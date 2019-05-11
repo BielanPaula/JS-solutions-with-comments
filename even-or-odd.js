204feaea3c1ef1761df04bb5b73d9937543348e3
BielanPaula <bielan.paula@gmail.com>
2019-05-11 12:32:10 +0200

    Add a solution for even or odd

---
+++ even-or-odd.js
@@ -0,0 +1,8 @@
+function even_or_odd(number) {
+  if (number % 2 == 0){  //if remainder will be equal 0 (the number is even when it's commensurate by 2)
+    return "Even"        //then return even
+  }
+  else {                 //if remainder will not be equal 0
+  return "Odd"           //then return odd
+  }
+}
